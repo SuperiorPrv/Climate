00d52f0103ef2e9845c8761e368975d2fafb882f
Superior_P <superior.prv@gmail.com>
2024-10-06 13:00:33 +0500

    header done

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,12 +1,16 @@
-import { Text } from "@mantine/core";
+import { Text, Burger, Drawer } from "@mantine/core";
 import { Link, useLocation } from "react-router-dom";
+import styles from "./header.module.css"
+import { useDisclosure } from "@mantine/hooks";
 
 const Header = () => {
   const {pathname} = useLocation();
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
-    <div className="" style={{ backgroundColor: "#EEEEEE" ,display:"flex",alignItems:"center",justifyContent:'space-between',padding:"12px 72px",flexWrap:"wrap"}}>
+    <div style={{ backgroundColor: "#EEEEEE" ,display:"flex",alignItems:"center",justifyContent:'space-between',padding:"12px 72px"}}>
       <Text  fw={500} size="36px">Climate</Text><br /><br />
-      <ul  style={{listStyle:"none",display:"flex",gap:"70px"}}>
+      <ul className={styles.nav}>
         <Link to="/"  style={pathname=="/"?{color:"black",fontSize:"24px",fontWeight:"600"}:{color:"black",textDecoration:"none",fontSize:"24px",fontWeight:"400"}}>
           <li>Home</li>
         </Link>
@@ -20,6 +24,23 @@ const Header = () => {
         <li>About</li>
         </Link>
       </ul>
+      <Burger onClick={open} className={styles.burger}/>
+      <Drawer size="40%" position="right" opened={opened} onClose={close}>
+        <ul style={{listStyle:"none"}}>
+          <Link to="/" onClick={()=>close()} style={pathname=="/"?{color:"black",fontSize:"24px",fontWeight:"600"}:{color:"black",textDecoration:"none",fontSize:"24px",fontWeight:"400"}}>
+            <li>Home</li>
+          </Link>
+          <Link to="/climate" onClick={()=>close()} style={pathname=="/climate"?{color:"black",fontSize:"24px",fontWeight:"600"}:{color:"black",textDecoration:"none",fontSize:"24px",fontWeight:"400"}}>
+            <li>Climate</li>
+          </Link>
+          <Link to="history" onClick={()=>close()} style={pathname=="/history"?{color:"black",fontSize:"24px",fontWeight:"600"}:{color:"black",textDecoration:"none",fontSize:"24px",fontWeight:"400"}}>
+            <li>History</li>
+          </Link>
+          <Link to="/about" onClick={()=>close()} style={pathname=="/about"?{color:"black",fontSize:"24px",fontWeight:"600"}:{color:"black",textDecoration:"none",fontSize:"24px",fontWeight:"400"}} >
+          <li>About</li>
+          </Link>
+        </ul>
+      </Drawer>
     </div>
   );
 };
